e53ac2839b0a36c936fecdffe11f413695e261fd
benjamin.daniel <benjamin.daniel@neo9.fr>
2016-11-16 16:36:46 +0100

    init sources files

---
+++ app/benj-beautify.js
@@ -0,0 +1,34 @@
+(function() {
+	var getNSpaces = function(n) {
+		var sArr = [];
+		for (; n > 0; n--) {
+			sArr[n] = "";
+		}
+		return sArr.join(" ");
+	};
+
+
+	window.benjbeautify = {
+		bean : function(textToFormat) {
+			var out = '';
+			var deep = 0;
+			for(var i = 0; i < textToFormat.length; i++) {
+				console.log(textToFormat[i]);
+				var letter = textToFormat[i];
+				if(letter === '(') {
+					deep++;
+					out += '(\n' + getNSpaces(deep);
+				}
+				else if(letter === ')') {
+					deep--;
+					out += '\n' + getNSpaces(deep) + ')';
+				} else if(letter === ','){
+					out += ',\n' + getNSpaces(deep);
+				} else {
+					out += letter;
+				}
+			}
+			return out;
+		}
+	};
+})();
