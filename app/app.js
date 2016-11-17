dc31ccae2798c0f96450f0ea1ba686eba34d6c3b
benjamin.daniel <benjamin.daniel@neo9.fr>
2016-11-17 17:53:31 +0100

    organise files

---
+++ app/app.js
@@ -0,0 +1,68 @@
+function selectText(id) {
+  var range = document.createRange();
+  range.selectNodeContents(document.getElementById(id));
+  var sel = window.getSelection();
+  sel.removeAllRanges();
+  sel.addRange(range);
+}
+
+function copyToClipboard(id) {
+  var elem = document.getElementById(id)
+  selectText(id);
+  var succeed;
+  try {
+    succeed = document.execCommand("copy");
+  } catch(e) {
+    succeed = false;
+  }
+  window.getSelection().removeAllRanges();
+  return succeed;
+}
+
+$(document).ready(function() {
+  window.updateCSSOut = function() {
+    var formatChoosen = $('input[name=format]:checked').val();
+    $('#pre').removeClass();
+    $('#pre').addClass(formatChoosen);
+    return formatChoosen;
+  };
+
+  function getInputVal() {
+    return $('#pre').text();
+  }
+
+  hljs.configure({
+    tabReplace: ' '
+  });
+
+  var format = function() {
+    $('#pre').text(getInputVal());
+    $('#pre').each(function(i, block) {
+      hljs.highlightBlock(block);
+    });
+  };
+
+  $('textarea').keyup(function() {
+    format();
+  });
+
+  $('#clear').click(function() {
+    $('#pre').html('');
+  });
+
+  $('#clearCdata').click(function() {
+    $('#pre').text(getInputVal().replace(/(<\!\[CDATA\[)|(\]\]>)/g,''));
+  });
+
+  $('#format').click(function() {
+    var formatChoosen = updateCSSOut();
+    if(formatChoosen === 'xml') {
+      $('#pre').text(vkbeautify.xml(getInputVal()));
+    } else if(formatChoosen === 'json') {
+      $('#pre').text(vkbeautify.json(getInputVal()));
+    } else if(formatChoosen === 'bean') {
+      $('#pre').text(benjbeautify.bean(getInputVal()));
+    }
+    format();
+  });
+});
